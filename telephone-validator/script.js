1275c5e5324846f15993e5651291fbfe75810f8a
CJ Grieco <cyberjenius2@gmail.com>
2024-11-26 18:08:46 -0500

    Restructured approach for code
    Restructured code, rolled back a few ideas involving input checking. Went back through and added regex for many expected valid numbers. Will fine tune with further testing and tests provided for project.

--- telephone-validator/script.js
+++ telephone-validator/script.js
@@ -3,35 +3,41 @@ const clearButton = document.getElementById("clear-btn");
 const userInput = document.getElementById("user-input");
 const results = document.getElementById("results-div");
 
-checkNumsOnly = (input) => {
-  if(input.includes(" ")){
-    let array = input.split(" ");
-    console.log(array);
-  } else {
-    if(input.length === 10){
-      const part1 = input.slice(0, 2);
-      const part2 = input.slice(3, 6);
-      const part3 = input.slice(7);
-    }else if(input.length === 11){
-      const part1 = input.getCharAt(0);
-      const part2 = input.slice(1, 3);
-      const part3 = input.slice(4, 7);
-      const part4 = input.slice(8)
-    }
-  }
-}
-
 const checkInput = () => {
-  console.log(userInput.value.match(/([0-9]+\s[0-9]+\s[0-9]+\s[0-9]+)|([0-9]+\s[0-9]+\s[0-9]+)|(\d+)/));
   if(userInput.value === ""){
     alert("Please provide a phone number");
-  } else if(userInput.value.match(/([0-9]+\s[0-9]+\s[0-9]+\s[0-9]+)|([0-9]+\s[0-9]+\s[0-9]+)|([0-9]+\s[0-9]+)|(d+)/)){
+  }else if(userInput.value.trim().match(/[1]\s[0-9][0-9][0-9]\s[0-9][0-9][0-9]\s[0-9][0-9][0-9][0-9]/)){
     console.log(userInput.value);
-    checkNumsOnly(userInput.value);
-  } else {
-    const numCharRegex = /[0-9\(\)\-]/g
-
+    return true;
+  } else if(userInput.value.trim().match(/[0-9][0-9][0-9]\s[0-9][0-9][0-9]\s[0-9][0-9][0-9][0-9]/)){
     console.log(userInput.value);
+    return true;
+  } else if(userInput.value.trim().match(/[1]\s[0-9][0-9][0-9]\-[0-9][0-9][0-9]\-[0-9][0-9][0-9][0-9]/)){
+    console.log(userInput.value);
+    return true;
+  } else if(userInput.value.trim().match(/[0-9][0-9][0-9]\-[0-9][0-9][0-9]\-[0-9][0-9][0-9][0-9]/)){
+    console.log(userInput.value);
+    return true;
+  } else if(userInput.value.trim().match(/[1]\s\([0-9][0-9][0-9]\)\s[0-9][0-9][0-9]\-[0-9][0-9][0-9][0-9]/)){
+    console.log(userInput.value);
+    return true;
+  } else if(userInput.value.trim().match(/[1]\s\([0-9][0-9][0-9]\)\s[0-9][0-9][0-9]\s[0-9][0-9][0-9][0-9]/)){
+    console.log(userInput.value);
+    return true;
+  }else if(userInput.value.trim().match(/[1]\([0-9][0-9][0-9]\)[0-9][0-9][0-9]\-[0-9][0-9][0-9][0-9]/)){
+    console.log(userInput.value)
+    return true;
+  } else if(userInput.value.trim().match(/\([0-9][0-9][0-9]\)[0-9][0-9][0-9]\-[0-9][0-9][0-9][0-9]/)){
+    console.log(userInput.value)
+    return true;
+  } else if(userInput.value.trim().match(/[1][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/)){
+    console.log(userInput.value);
+    return true;
+  } else if(userInput.value.trim().match(/[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/)){
+    console.log(userInput.value);
+    return true;
+  }else {
+    return false;
   }
 }
 
@@ -39,5 +45,15 @@ const clearResults = () => {
   results.textContent = "";
 }
 
-checkButton.addEventListener("click", checkInput);
-checkButton.addEventListener("click", clearResults);
+const checkResults = () => {
+  if(checkInput()){
+    results.textContent += (`Valid US number: ${userInput.value}` + "\n");
+    return;
+  }else{
+    results.textContent += (`Invalid US number: ${userInput.value}` + "\n");
+    return;
+  }
+}
+
+checkButton.addEventListener("click", checkResults);
+clearButton.addEventListener("click", clearResults);
