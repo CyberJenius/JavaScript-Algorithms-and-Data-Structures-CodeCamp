341b0eae05dc8b43eea8bca32775eda88ccd4f93
CJ Grieco <cyberjenius2@gmail.com>
2024-11-29 21:58:48 -0500

    Changed checking
    Changed checking and regex statements again for slight simplicity added other checks below regarding starting with special characters or the correct area code.

--- telephone-validator/script.js
+++ telephone-validator/script.js
@@ -7,10 +7,22 @@ const checkInput = () => {
   if(userInput.value === ""){
     alert("Please provide a phone number");
     return;
-  }else if(userInput.value.trim().match((/[1]?\s?\(?\d{3}\)?\s?\d{3}\s?\d{4}/)|(/[1]?\s?\(?\d{3}\)?\-?\d{3}\-?\d{4}/))){
-    console.log(userInput.value);
-    return true;
+  }else if(!userInput.value.charAt(0).match(/d/g)){
+    return false;
+  }else if(userInput.value.trim().match(/([1]?\s?\(\d{3}\)\s?\d{3}\s?\d{4})|([1]?\s?\(\d{3}\)\-?\d{3}\-?\d{4})|([1]?\s?\(\d{3}\)\s?\d{3}\-?\d{4})|([1]?\s?\d{3}\s?\d{3}\s?\d{4})|([1]?\s?\d{3}\-?\d{3}\-?\d{4})|([1]?\s?\d{3}\)\s?\d{3}\-?\d{4})/g)){
+    let input = userInput.value.trim().replace(/[^\w]/g,"");
+    if(input.length === 11 && input.charAt(0) === "1"){
+      console.log(input);
+      return true;
+    } else if(input.length === 10){
+      console.log(input);
+      return true;
+    } else {
+      console.log(input);
+      return false;
+    }
   }else{
+    console.log(userInput.value);
     return false;
   }
 }
@@ -22,6 +34,8 @@ const clearResults = () => {
 const checkResults = () => {
   if(checkInput()){
     results.innerHTML += (`<br>Valid US number: ${userInput.value}`);
+    return;
+  }else if(userInput.value === ""){
     return;
   }else{
     results.innerHTML += (`<br>Invalid US number: ${userInput.value}`);
