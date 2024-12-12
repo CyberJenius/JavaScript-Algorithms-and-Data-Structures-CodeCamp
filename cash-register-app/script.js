49daa08ab2fdfd50ca1f6053b35a1ffbeb230987
CJ Grieco <cyberjenius2@gmail.com>
2024-12-12 14:53:36 -0500

    Finished basic App visualization.
    Added some html and css to get visual to look a way I liked a good bit and that fits the project.

--- cash-register-app/script.js
+++ cash-register-app/script.js
@@ -15,5 +15,22 @@ let cid = [
 const cashInput = document.getElementById("cash");
 const changeDue = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
+const total = document.getElementById("total");
+const changeInDrawer = document.getElementById("change");
+
+total.innerHTML += `${price}`;
+changeInDrawer.innerHTML += `<br> Penny: ${cid[0][1]}
+ <br> Nickel: ${cid[1][1]}
+ <br> Dime: ${cid[2][1]}
+ <br> Quarter: ${cid[3][1]}
+ <br> One: ${cid[4][1]}
+ <br> Five: ${cid[5][1]} 
+ <br> Ten: ${cid[6][1]}
+ <br> Twenty: ${cid[7][1]}
+ <br> One Hundred: ${cid[8][1]}`
+
+ const purchaseItem = () => {
+  
+ }
 
 
