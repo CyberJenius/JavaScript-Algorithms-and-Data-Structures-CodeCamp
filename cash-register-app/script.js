e460b564dd8069a8e6fcada11358adc8b7e2bb42
CJ Grieco <cyberjenius2@gmail.com>
2024-12-20 12:56:52 -0500

    Began tweaking program for tests.
    Began making small changes to it tests from FreeCodeCamp including messing with tests and fixing typos.

--- cash-register-app/script.js
+++ cash-register-app/script.js
@@ -12,6 +12,8 @@ let cid = [
 ];
 //above variables provided by FreeCodeCamp
 
+let totalCID = cid[0][1] + cid[1][1] + cid[2][1] + cid[3][1] + cid[4][1] + cid[5][1] + cid[6][1] + cid[7][1] + cid[8][1];
+
 const cashInput = document.getElementById("cash");
 const changeDue = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
@@ -39,15 +41,24 @@ changeInDrawer.innerHTML += `<br> Penny: ${cid[0][1]}
   let i = 8;
     moneyVals.forEach((val)=> {
       if(difference / val > 1 && cid[i][1] > 0){
-        cid[i][1] = cid[i][1] - (difference / val) * val;
-        checkArr.push(Math.floor(difference / val) * val);
-        changeArr.push(cid[i][0] + ": " + (Math.floor(difference / val) * val));
-        difference = (difference - Math.floor(difference / val) * val).toFixed(2);
+        totalCID += cid[i][1];
+        if((Math.floor(difference / val) * val) > cid[i][1]){
+          checkArr.push(cid[i][1]);
+          changeArr.push(cid[i][0] + ": $" + cid[i][1]);
+          difference = difference - cid[i][1];
+          totalCID -= cid[i][1];
+          cid[i][1] = 0;
+        }else {
+          cid[i][1] = cid[i][1] - Math.floor(difference / val) * val;
+          checkArr.push(Math.floor(difference / val) * val);
+          changeArr.push(cid[i][0] + ": $" + (Math.floor(difference / val) * val));
+          difference = (difference - Math.floor(difference / val) * val).toFixed(2);
+          totalCID -= Math.floor(difference / val) * val;
+        }
       } 
       i--;
-    })   
-
-    if(checkArr.reduce((a, b) => a + b, 0) != startDif || difference > 0){
+    })
+    if(parseFloat(checkArr.reduce((a, b) => a + b, 0).toFixed(2)) != startDif || difference > 0 || totalCID < startDif){
       return `Status INSUFFICIENT_FUNDS`;
     }else if(cid[0][1] === 0 && cid[1][1] === 0 && cid[2][1] === 0 && cid[3][1] === 0 && cid[4][1] === 0 && cid[5][1] === 0 && cid[6][1] === 0 && cid[7][1] === 0 && cid[8][1] === 0){
       let returnString = "Status: CLOSED ";
@@ -65,15 +76,15 @@ changeInDrawer.innerHTML += `<br> Penny: ${cid[0][1]}
  }
 
  const updateDrawer = () => {
-  changeInDrawer.innerHTML += `<br> Penny: ${cid[0][1]}
-  <br> Nickel: ${cid[1][1]}
-  <br> Dime: ${cid[2][1]}
-  <br> Quarter: ${cid[3][1]}
-  <br> One: ${cid[4][1]}
-  <br> Five: ${cid[5][1]} 
-  <br> Ten: ${cid[6][1]}
-  <br> Twenty: ${cid[7][1]}
-  <br> One Hundred: ${cid[8][1]}`
+  changeInDrawer.innerHTML = `<br> Penny: ${parseFloat(cid[0][1].toFixed(2))}
+  <br> Nickel: ${parseFloat(cid[1][1].toFixed(2))}
+  <br> Dime: ${parseFloat(cid[2][1].toFixed(2))}
+  <br> Quarter: ${parseFloat(cid[3][1].toFixed(2))}
+  <br> One: ${parseFloat(cid[4][1].toFixed(2))}
+  <br> Five: ${parseFloat(cid[5][1].toFixed(2))} 
+  <br> Ten: ${parseFloat(cid[6][1].toFixed(2))}
+  <br> Twenty: ${parseFloat(cid[7][1].toFixed(2))}
+  <br> One Hundred: ${parseFloat(cid[8][1].toFixed(2))}`
  }
 
  purchaseBtn.addEventListener("click", () => {
@@ -81,7 +92,7 @@ changeInDrawer.innerHTML += `<br> Penny: ${cid[0][1]}
     alert("Customer does not have enough money to purchase the item")
     return;
   }else if(Number(cashInput.value) === price){
-    changeDue.textContent = "No change due - customer payed with exact cash";
+    changeDue.textContent = "No change due - customer paid with exact cash";
     return;
   }else{
     changeDue.textContent = calculateChange(cashInput.value);
