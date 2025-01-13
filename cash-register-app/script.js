2feb0828a7548f09b325d60edfdafd9556ebb030
CJ Grieco <cyberjenius2@gmail.com>
2025-01-13 13:20:44 -0500

    First attempt at refactoring/editing variables to fix bug
    Changed variables, added a intake for the global variable, edited math functions to fit new variable, made changes involving if statements, still needs lots of work.

--- cash-register-app/script.js
+++ cash-register-app/script.js
@@ -1,18 +1,7 @@
-let price = 1.87;
-let cid = [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100]
-];
+let price = 19.5;
+let cid = [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]
 //above variables provided by FreeCodeCamp
 
-let totalCID = cid[0][1] + cid[1][1] + cid[2][1] + cid[3][1] + cid[4][1] + cid[5][1] + cid[6][1] + cid[7][1] + cid[8][1];
 
 const cashInput = document.getElementById("cash");
 const changeDue = document.getElementById("change-due");
@@ -32,35 +21,47 @@ changeInDrawer.innerHTML += `<br> Penny: ${cid[0][1]}
  <br> One Hundred: ${cid[8][1]}`
 
 
- const calculateChange = (funds) => {
-  const startDif = funds - price;
+ const calculateChange = (funds, newCID) => {
+  let totalCID = newCID[0][1] + newCID[1][1] + newCID[2][1] + newCID[3][1] + newCID[4][1] + newCID[5][1] + newCID[6][1] + newCID[7][1] + newCID[8][1];
+
+  const startDif = parseFloat(funds - price).toFixed(2);
   let difference = funds - price;
   const moneyVals = [100, 20, 10, 5, 1, .25, .10, .05, .01];
   let changeArr = [];
   let checkArr = [];
   let i = 8;
-    moneyVals.forEach((val)=> {
-      if(difference / val > 1 && cid[i][1] > 0){
-        totalCID += cid[i][1];
-        if((Math.floor(difference / val) * val) > cid[i][1]){
-          checkArr.push(cid[i][1]);
-          changeArr.push(cid[i][0] + ": $" + cid[i][1]);
-          difference = difference - cid[i][1];
-          totalCID -= cid[i][1];
-          cid[i][1] = 0;
-        }else {
-          cid[i][1] = cid[i][1] - Math.floor(difference / val) * val;
-          checkArr.push(Math.floor(difference / val) * val);
-          changeArr.push(cid[i][0] + ": $" + (Math.floor(difference / val) * val));
-          difference = (difference - Math.floor(difference / val) * val).toFixed(2);
-          totalCID -= Math.floor(difference / val) * val;
-        }
-      } 
-      i--;
-    })
-    if(parseFloat(checkArr.reduce((a, b) => a + b, 0).toFixed(2)) != startDif || difference > 0 || totalCID < startDif){
+  console.log(parseFloat(totalCID).toFixed(2));
+    if(parseFloat(totalCID).toFixed(2) >= parseFloat(startDif)){ 
+        moneyVals.forEach((val)=> {
+        if(difference / val > 1 && newCID[i][1] > 0){
+          if((Math.floor(difference / val) * val) > newCID[i][1]){
+            checkArr.push(newCID[i][1]);
+            changeArr.push(newCID[i][0] + ": $" + newCID[i][1]);
+            difference = difference - newCID[i][1];
+            totalCID = parseFloat(parseFloat(totalCID - newCID[i][1]).toFixed(2));
+            newCID[i][1] = 0;
+          }else {
+            newCID[i][1] = parseFloat(parseFloat(newCID[i][1] - Math.floor(difference / val) * val).toFixed(2));
+            checkArr.push(Math.floor(difference / val) * val);
+            changeArr.push(newCID[i][0] + ": $" + (Math.floor(difference / val) * val));
+            totalCID -= difference / val * val;
+            difference = parseFloat(difference - Math.floor(difference / val) * val).toFixed(2);
+          }
+        } 
+        i--;
+      })
+    } else {
       return `Status: INSUFFICIENT_FUNDS`;
-    }else if(cid[0][1] === 0 && cid[1][1] === 0 && cid[2][1] === 0 && cid[3][1] === 0 && cid[4][1] === 0 && cid[5][1] === 0 && cid[6][1] === 0 && cid[7][1] === 0 && cid[8][1] === 0){
+    }
+
+    console.log(difference);
+
+    console.log(changeArr);
+    console.log(startDif);
+    console.log(totalCID);
+    console.log(parseFloat(checkArr.reduce((a, b) => a + b, 0).toFixed(2)));
+    
+    if(totalCID === 0 || parseFloat(totalCID).toFixed(2) === startDif){
       let returnString = "Status: CLOSED ";
       changeArr.forEach((val) => {
         returnString += val + " ";
@@ -75,29 +76,31 @@ changeInDrawer.innerHTML += `<br> Penny: ${cid[0][1]}
     }
  }
 
- const updateDrawer = () => {
-  changeInDrawer.innerHTML = `<br> Penny: ${parseFloat(cid[0][1].toFixed(2))}
-  <br> Nickel: ${parseFloat(cid[1][1].toFixed(2))}
-  <br> Dime: ${parseFloat(cid[2][1].toFixed(2))}
-  <br> Quarter: ${parseFloat(cid[3][1].toFixed(2))}
-  <br> One: ${parseFloat(cid[4][1].toFixed(2))}
-  <br> Five: ${parseFloat(cid[5][1].toFixed(2))} 
-  <br> Ten: ${parseFloat(cid[6][1].toFixed(2))}
-  <br> Twenty: ${parseFloat(cid[7][1].toFixed(2))}
-  <br> One Hundred: ${parseFloat(cid[8][1].toFixed(2))}`
+ const updateDrawer = (newCID) => {
+  changeInDrawer.innerHTML = `<br> Penny: ${parseFloat(newCID[0][1]).toFixed(2)}
+  <br> Nickel: ${parseFloat(newCID[1][1]).toFixed(2)}
+  <br> Dime: ${parseFloat(newCID[2][1]).toFixed(2)}
+  <br> Quarter: ${parseFloat(newCID[3][1]).toFixed(2)}
+  <br> One: ${parseFloat(newCID[4][1]).toFixed(2)}
+  <br> Five: ${parseFloat(newCID[5][1]).toFixed(2)} 
+  <br> Ten: ${parseFloat(newCID[6][1]).toFixed(2)}
+  <br> Twenty: ${parseFloat(newCID[7][1]).toFixed(2)}
+  <br> One Hundred: ${parseFloat(newCID[8][1]).toFixed(2)}`
  }
 
  purchaseBtn.addEventListener("click", () => {
+  let newCID = [...cid];
+  console.log(newCID);
   if(cashInput.value < price){
     alert("Customer does not have enough money to purchase the item")
     return;
-  }else if(Number(cashInput.value) === price){
-    changeDue.textContent = "No change due - customer paid with exact cash";
+  }else if(price === Number(cashInput.value)){
+    changeDue.innerHTML = "No change due - customer paid with exact cash";
     return;
-  }else{
-    changeDue.textContent = calculateChange(cashInput.value);
+  }else if(price < cashInput.value){
+    changeDue.innerHTML = calculateChange(parseFloat(cashInput.value), newCID);
   }
-  updateDrawer();
+  updateDrawer(newCID);
  });
 
 
